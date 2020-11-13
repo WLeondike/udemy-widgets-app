f5a110310e3555cf2af6d33f1d96650966d17ce4
WLeondike <billyleondike@gmail.com>
2020-11-13 18:12:15 -0500

    started working on Accordion component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordian from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end JavaScript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JavaScript library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+];
+
+export default () => {
+  return (
+    <div>
+      <Accordian items={items} />
+    </div>
+  );
+};
