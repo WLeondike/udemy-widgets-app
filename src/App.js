499dc0bc9300c13e28c306a64f0725a9cc3940c6
WLeondike <billyleondike@gmail.com>
2020-11-17 16:47:02 -0500

    started search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Accordian from './components/Accordion';
+// import Accordian from './components/Accordion';
+import Search from './components/Search'
 
 const items = [
   {
@@ -19,7 +20,8 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordian items={items} />
+      {/* <Accordian items={items} /> */}
+      <Search />
     </div>
   );
 };
