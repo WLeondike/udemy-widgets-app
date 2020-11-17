348be79b7961ce95ca448427fa09e93566334a44
WLeondike <billyleondike@gmail.com>
2020-11-17 17:31:26 -0500

    added in axios/getting results back from wiki api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const searchWiki = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+    searchWiki();
+  });
+
   return (
     <div>
       <div className='ui form'>
         <div className='field'>
           <label>Enter Search Term</label>
-          <input className='input' />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className='input'
+          />
         </div>
       </div>
     </div>
