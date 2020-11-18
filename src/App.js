5330af4729abe7d6898ed0988dd0207f7ff9b02b
WLeondike <billyleondike@gmail.com>
2020-11-18 11:22:11 -0500

    added Dropdown component, base structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
 // import Accordian from './components/Accordion';
-import Search from './components/Search'
+// import Search from './components/Search'
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
     title: 'What is React?',
-    content: 'React is a front end JavaScript framework'
+    content: 'React is a front end JavaScript framework',
   },
   {
     title: 'Why use React?',
-    content: 'React is a favorite JavaScript library among engineers'
+    content: 'React is a favorite JavaScript library among engineers',
   },
   {
     title: 'How do you use React?',
-    content: 'You use React by creating components'
-  }
+    content: 'You use React by creating components',
+  },
+];
+
+const options = [
+  {
+    label: `I'm Red!`,
+    value: 'red',
+  },
+  {
+    label: `I'm Green!`,
+    value: 'green',
+  },
+  {
+    label: `I'm Blue!`,
+    value: 'blue',
+  },
 ];
 
 export default () => {
   return (
     <div>
       {/* <Accordian items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
