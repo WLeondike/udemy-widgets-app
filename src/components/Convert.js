940649e5c4629da7a424a122ac0318994bee6035
WLeondike <billyleondike@gmail.com>
2020-11-19 15:10:20 -0500

    getting the translation posted to page

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,20 +5,30 @@ import axios from 'axios';
 const KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: KEY,
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
-  return <div />;
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
